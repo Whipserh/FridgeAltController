abaf448661b5a7278589153af541fd13b9256ea6
Ryan <mcbrider@sheridancollege.ca>
2026-04-09 11:52:23 -0400

    Progress on fridge controller

--- FridgeController/FridgeController.ino
+++ FridgeController/FridgeController.ino
@@ -1,56 +1,134 @@
 #include <Servo.h>
-Servo myServo;
 
-int const potPin = A0;
+
+//circuit board component variablles
+const int potPin = A0;
+const int pot2Pin = A1;
+const int sliderPot = A2;
+const int slider2Pot = A3;
 int potVal;
-int angle;
+int pot2Val;
+int sliderVal;
+int slider2Val;
 
+
+const int ledPin = 13;
+const int led2Pin = 12;
+const int led3Pin = 11;
+const int led4Pin = 10;
+
+//fan turner componenets
+Servo fanServo;
+const int fanServoPin = 5;
+
+int fanAngle;
+
+const int leftSwitchPin = 6;
+const int rightSwitchPin = 7;
+
+//these overview the state of the switches
+int leftSwitchState = 0;
+int previousLeftSwitchState = 0;
+int rightSwitchState = 0;
+int previousRightSwitchState = 0;
+
+//thermometer component variabels
+Servo thermoServo;
+const int thermoServoPin = 8;
+int thermometerVal;
+int thermometerAngle;
+
+
+//fan component variables
+const int enablePin = 4;
 const int controlPin1 = 2;
 const int controlPin2 = 3;
-const int enablePin = 9;
-const int directionSwitchPin = 4;
-const int onOffSwitchStateSwitchPin = 5;
-//const int potPin = A0;
-
-int onOffSwitchState = 0;
-int previousOnOffSwitchState = 0;
-int directionSwitchState = 0;
-int previousDirectionSwitchState = 0;
 
 int motorEnabled = 0;
 int motorSpeed = 0;
 int motorDirection = 1;
 
-void setup() {
-  myServo.attach(9);
-  Serial.begin(9600);
 
-  /*
-  pinMode(directionSwitchPin, INPUT);
-  pinMode(onOffSwitchStateSwitchPin, INPUT);
+
+
+void setup() {
+  //REMINDER: you don't need to set up pot pins in the analog area
+
+  //set up fan turner components
+  fanServo.attach(fanServoPin);
+  //controls fot fan
+  pinMode(leftSwitchPin, INPUT);
+  pinMode(rightSwitchPin, INPUT);
+
+  //set up thermoComponents
+  thermoServo.attach(thermoServoPin);
+
+  //set up the fan motor
   pinMode(controlPin1, OUTPUT);
   pinMode(controlPin2, OUTPUT);
   pinMode(enablePin, OUTPUT);
+
+  //set the motor to Off
   digitalWrite(enablePin, LOW);
-  */
+
+  //comunicate to unity project
+  Serial.begin(115200);
+}
+
+private
+void controlMotorSpeed(bool enableMotor, int motorSpeed) {
+  //turn motor on/off
+  if (enableMotor) digitalWrite(enablePin, HIGH);
+  else digitalWrite(enablePin, LOW);
+
+  //set speed of motor
+}
+
+//write to data packet
+private void sendDataPacket() {
+  string a = "{";
+}
+
+//reads from unity program
+private void readDataPacket() {
+  //thermometerVal = ;
+}
+
+private void readCircuitInput(){
+  potVal = analogRead(potPin);
+  pot2Val = analogRead(pot2Pin);
+  sliderVal = analogRead(sliderPot);
+  slider2Val = analogRead(slider2Pot);
 }
 
 void loop() {
+  //read the state of the left/right control switch states
+  leftSwitchState = digitalRead(leftSwitchPin);
+  rightSwitchState = digitalRead(rightSwitchPin);
 
-  //code for servo
-  potVal = analogRead(potPin);
-  Serial.print("potVal: ");
-  Serial.print(potVal);
-  angle = map(potVal, 0, 1023, 0, 179);
-  Serial.print(", angle: ");
-  Serial.println(angle);
+  //read curcuit board inputs
+  readCircuitInput();
+
+  //write the angle and send it too the thing
+  angle = map(potVal, 0, 1023, 0, 179);//lerp function from 0-1023 to total range of 0-180 degrees
   myServo.write(angle);
   delay(15);
+
+  //update thermometer
+  thermometerAngle = map(thermometerVal, 0, 2, 0, 179);
+  thermoServo.write(thermometerAngle);
+  //update motor controls
+  controlMotorSpeed(enableMotor, motorSpeed);
+
+  //update Fan turner controls
+  fanServo.write(fanAngle);  
   /*
   // put your main code here, to run repeatedly:
   onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
   delay(1);
   directionSwitchState = digitalRead(directionSwitchPin);
+
+  //reads pot to set speed of motor
   motorSpeed = analogRead(potPin)/4;
 
   if(onOffSwitchState != previousOnOffSwitchState){
@@ -84,4 +162,8 @@ void loop() {
   previousDirectionSwitchState = directionSwitchState;
   previousOnOffSwitchState = onOffSwitchState;
   */
-}//end loop
+  previousLeftSwitchState = leftSwitchState;
+  previousRightSwitchState = rightSwitchState;
+
+
+}  //end loop
